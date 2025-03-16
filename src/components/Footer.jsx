20479f7411ca4799e11445200310c2aa81cc5586
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-03-16 23:38:41 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white text-center p-4">
-      <p>Made by <span className="text-blue-400">Hakkan</span></p>
+      <p>Made by<span className="text-blue-400">Hakkan</span></p>
     </footer>
   );
 };
