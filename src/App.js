fd72fe6f641f3ed562c8faf22b8aaceb50153682
Hakkan Parbej Shah <hakkanparbej@gmail.com>
2025-06-13 19:31:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
 function App() {
   return (
     <>
